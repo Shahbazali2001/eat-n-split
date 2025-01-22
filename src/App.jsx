61f9829d9bf5548ffd4c14ac254eb7d61728dfd2
Shahbazali2001 <shahbazali5097855@gmail.com>
2025-01-22 15:00:43 +0500

    Project Initialized

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+
+function App() {
+ 
+
+  return (
+    <>
+      <div>
+        <h1 className='text-center text-bold text-5xl'>Eat-n-Split</h1>
+      </div>
+       
+    </>
+  )
+}
+
+export default App
